5a3279d7cfc01849cfd081f2b560c46b3bf33ed3
Arseniy Graur <argraur12@yandex.ru>
2017-04-23 09:20:27 +0300

    Add VendorLinks

--- Android.mk
+++ Android.mk
@@ -39,3 +39,26 @@ LOCAL_DEX_PREOPT := false
 LOCAL_SRC_FILES := proprietary/app/TimeService/$(LOCAL_MODULE).apk
 LOCAL_PRIVILEGED_MODULE := false
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := VendorLinks
+LOCAL_MODULE_OWNER := huawei
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_POST_INSTALL_CMD := \
+    test -s vendor/huawei/angler/proprietary/prebuilt/target/product/msm8994/system/etc/izat.conf || { \
+    mkdir -p $(PRODUCT_OUT)/system/app/ims/lib/arm64; \
+    ln -sf /vendor/app/lib64/libimscamera_jni.so $(PRODUCT_OUT)/system/app/ims/lib64/libimscamera_jni.so; \
+    ln -sf /vendor/app/lib64/libimsmedia_jni.so $(PRODUCT_OUT)/system/app/ims/lib64/libimsmedia_jni.so; \
+    mkdir -p $(PRODUCT_OUT)/vendor/etc/firmware/wcd9320; \
+    ln -sf /data/misc/audio/wcd9320_anc.bin $(PRODUCT_OUT)/vendor/etc/firmware/wcd9320/wcd9320_anc.bin; \
+    ln -sf /data/misc/audio/wcd9320_mad_audio.bin $(PRODUCT_OUT)/vendor/etc/firmware/wcd9320/wcd9320_mad_audio.bin; \
+    ln -sf /data/misc/audio/wcd9320_mbhc.bin $(PRODUCT_OUT)/vendor/etc/firmware/wcd9320/wcd9320_mbhc.bin; \
+    mkdir -p $(PRODUCT_OUT)/vendor/lib64; \
+    ln -sf /vendor/lib64/egl/eglSubDriverAndroid.so $(PRODUCT_OUT)/vendor/lib64/eglSubDriverAndroid.so; \
+    ln -sf /vendor/lib64/egl/libEGL_adreno.so $(PRODUCT_OUT)/vendor/lib64/libEGL_adreno.so; \
+    ln -sf /vendor/lib64/egl/libGLESv1_CM_adreno.so $(PRODUCT_OUT)/vendor/lib64/libGLESv1_CM_adreno.so; \
+    ln -sf /vendor/lib64/egl/libGLESv2_adreno.so $(PRODUCT_OUT)/vendor/lib64/libGLESv2_adreno.so }
+
+include $(BUILD_PHONY_PACKAGE)
