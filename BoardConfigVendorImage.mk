70fc4d4a42505adcdef35dc31d050afebe68f3ba
argraur <argraur@gmail.com>
2017-04-25 15:07:35 +0000

    Move variables to BoardConfigVendorImage.mk from device source

--- BoardConfigVendorImage.mk
+++ BoardConfigVendorImage.mk
@@ -1,17 +1,19 @@
-# Copyright 2015 The Android Open Source Project
+# Copyright (C) 2017 The Google PixelROM Project (AOSP)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Include vendor image build
-LOCAL_STEM := angler/BoardConfigVendorImage.mk
 
--include vendor/huawei/$(LOCAL_STEM)
+# Build vendor image from source
+BOARD_VENDORIMAGE_PARTITION_SIZE := 209715200
+
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
